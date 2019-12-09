90c521f5362c25ff01d94d5b20662ad884c55932
denhampreen <denhampreen@gmail.com>
2019-12-09 21:38:46 +0100

    socials

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./Footer.css";
+import ReactDOM from "react-dom";
+import { SocialIcon } from "react-social-icons";
 
 const Footer = () => {
   return (
     <footer className="bg-black text-center text-white-50 footer">
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-8 col-lg-8 col-sm-12  col-xs-12 mx-auto">
+          <div
+            className="col-md-8
+           col-lg-8 col-sm-12  col-xs-12 mx-auto"
+          >
             <p>
               This website is not an invitation to engage in investment
               activity. Clima.Investments will cater to UK residents only and
@@ -16,6 +21,11 @@ const Footer = () => {
                 {" "}
                 info@clima.investments
               </a>
+            </p>
+            <p>
+              <SocialIcon url="https://www.facebook.com/climainvestments" />{" "}
+              <SocialIcon url="https://www.linkedin.com/company/64253548" />{" "}
+              <SocialIcon url="https://www.instagram.com/climainvestments/" />
             </p>
           </div>
         </div>

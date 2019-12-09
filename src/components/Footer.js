abc0f971f68d95029b211c7c924911acbf1e622a
denhampreen <denhampreen@gmail.com>
2019-12-09 18:51:03 +0100

    footer responsive accross all sizes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,20 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-center text-white-50">
-      <div className="container">
+    <footer className="bg-black text-center text-white-50 footer">
+      <div className="container-fluid">
         <div className="row">
-          <div className="col-md-10  col-sm-12  col-xs-12 mx-auto">
-            This website is not an invitation to engage in investment activity.
-            Clima.Investments will cater to UK residents only and will be
-            regulated by the FCA. If you have any enquiries please contact{" "}
-            <a href="mailto:info@clima.investments"> info@clima.investments</a>
+          <div className="col-md-10 col-lg-10 col-sm-12  col-xs-12 mx-auto">
+            <p>
+              This website is not an invitation to engage in investment
+              activity. Clima.Investments will cater to UK residents only and
+              will be regulated by the FCA. If you have any enquiries please
+              contact{" "}
+              <a href="mailto:info@clima.investments">
+                {" "}
+                info@clima.investments
+              </a>
+            </p>
           </div>
         </div>
       </div>
